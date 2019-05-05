68dc617382cb8c731a6a8da563e40a614c6b3f99
Rajiv Bharadwaj <rajivbh@umich.edu>
2019-05-05 14:40:56 -0400

    Updates to encryptor
    However, modulus still doesn't work. Planning on creating a modulus test.

--- encryptor.py
+++ encryptor.py
@@ -1,14 +1,21 @@
 import math
 import string
+from mod import Mod
 
 alphabets = dict(zip(string.ascii_lowercase, range(0,26)))
 print(alphabets)
-with open('keys.rsaconf', 'r') as keyfile:
-    n = keyfile.readline()
-    e = keyfile.readline()
+with open('publickeys.rsaconf', 'r') as keyfile:
+    n = int(keyfile.readline())
+    e = int(keyfile.readline())
     
     message = input("Enter message to encrypt ")
     numMessage = []
+    encryptedMessage = []
     for char in message:
         numMessage.append(alphabets[char])
-    print(numMessage)     
+    for char in numMessage:
+        encryptedChar = char
+        for i in range(1,e):
+            encryptedChar = (encryptedChar*char) % n
+        encryptedMessage.append(encryptedChar)
+    print(encryptedMessage)
